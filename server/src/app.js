77fd1adc34507025cb7a16f124797f760fadca84
imran <izaheer612@gmail.com>
2022-07-17 01:43:52 +0500

    deploy test

--- server/src/app.js
+++ server/src/app.js
@@ -6,10 +6,10 @@ const app = express();
 if (process.env.NODE_ENV === "production") {
     const path = require('path');
 
-    app.use(express.static('../../client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../', '../', 'client', 'build', 'index.html'));
-    });
+    app.use(express.static(path.join(__dirname, '../../client/build')));
+    // app.get('*', (req, res) => {
+        // res.sendFile(path.resolve(__dirname, '../../client/build/index.html'));
+    // });
 }
 
 
