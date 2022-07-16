34636890bbde8eba80d6ac9fc296ab2961c658e7
imran <izaheer612@gmail.com>
2022-07-17 00:57:14 +0500

    deploy testing

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,17 @@ const stories = require('./routes/stories');
 const express = require('express');
 const app = express();
 
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+
+    app.use(express.static('../../client/build'));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '../', '../', 'client', 'build', 'index.html'));
+    });
+}
+
+
 app.use(express.json());
 app.use('/api/story', stories);
 
--- server/src/server.js
+++ server/src/server.js
@@ -15,16 +15,6 @@ mongoose.connection.on('error', (error) => {
     console.log(error);
 });
 
-// Server static assets if in production
-
-if (process.env.NODE_ENV === "production") {
-
-    app.use(express.static('../../client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, '../', '../', 'client', 'build', 'index.html'));
-    });
-}
-
 
 const port = process.env.PORT;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
