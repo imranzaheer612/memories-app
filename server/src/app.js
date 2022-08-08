e9a2469969004d8fa2b4b2fc2855476149f77b62
imran <izaheer612@gmail.com>
2022-08-08 22:50:15 +0500

    react router issue

--- server/src/app.js
+++ server/src/app.js
@@ -23,9 +23,9 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/build')));
 
-  // app.get('/*', (req, res) => {
-  //   res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
-  // });
+  app.get('/*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../../client/build/index.html'));
+  });
 }
 
 /**
