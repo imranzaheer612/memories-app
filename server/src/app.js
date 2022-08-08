f1483e154d41229e53d75ee98a4dbc0f398b928d
imran <izaheer612@gmail.com>
2022-08-08 22:12:01 +0500

    route issue server

--- server/src/app.js
+++ server/src/app.js
@@ -22,6 +22,10 @@ const app = express();
  */
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../../client/build', 'index.html'));
+  });
 }
 
 /**
